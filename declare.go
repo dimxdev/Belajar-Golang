5898ccd192e043dccce6b610ff40bac6766048f5
Dimas Brotowali Hidayatullah <brotowalidimas@gmail.com>
2026-08-29 10:08:24 +0800

    -

--- declare.go
+++ declare.go
@@ -3,5 +3,5 @@ package main
 import "fmt"
 
 func declare() {
-	fmt.Println("Halo, aku mulai belajar Go dari nol 22/08/2026 16.20")
+	fmt.Println("Halo, aku mulai belajar Go dari nol 02/08/2026 16.20")
 }
